faf4df37c63dec8cded569cf57889b1c3168cb07
AmoGlobine <dennisaubryy@gmail.com>
2024-07-07 00:05:22 +0200

    update projects preview

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,12 +11,15 @@ const Projects = () => {
   return (
     <div className="works">
     <div className="works__animation">
+      <h3>Click pour aperçu du site</h3>
             <div className="works__projects">
       {project.map((project, index) => (
         <div className="works__cards" key={index}>
+          <a href={project.link}>
           <h2>{project.title}</h2>
           <img src={project.image} alt={project.title} />
           <p>{project.description}</p>
+          </a>
           <a href={project.git} target="_blank" rel="noopener noreferrer">
             View on GitHub
           </a>
