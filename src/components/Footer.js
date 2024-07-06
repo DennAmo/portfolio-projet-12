710e4a63022200d0199bc1f0fda0a12adf09f5d6
AmoGlobine <dennisaubryy@gmail.com>
2024-07-06 23:40:14 +0200

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Nav from "./Nav";
+import MatrixText from "./MatrixText.js";
 
 const Footer = () => {
   return (
@@ -8,6 +9,9 @@ const Footer = () => {
       <h4>Navigation Rapide</h4>
       <Nav />
 <h5>Aubry Dennis © 2024</h5>
+<a target="_blank" rel="noreferrer" href="https://www.youtube.com/watch?v=ESx_hy1n7HA"><MatrixText text=" me.escape(world);
+world.execute(me);" />
+</a>
       </footer>
     </>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,10 @@
 import React from "react";
 import Nav from "./Nav";
-import MatrixText from "./MatrixText.js";
 
 const Header = () => {
   return (
     <header>
     <h3>Portfolio</h3>
-    <MatrixText text=" me.escape(world);
-       world.execute(me);" />
       <Nav />
     </header>
   );
