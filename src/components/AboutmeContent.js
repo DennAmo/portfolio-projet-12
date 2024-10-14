94889bc81593c5ef8ac310c959a3ae8e0afea21e
AmoGlobine <dennisaubryy@gmail.com>
2024-10-14 11:23:40 +0200

    update aboutme

--- src/components/AboutmeContent.js
+++ src/components/AboutmeContent.js
@@ -8,36 +8,19 @@ const AboutmeContent = () => {
       <a href="https://chat.google.com/room/AAAAvaJ2eyU?cls=7">Google Chat</a>
 
       <h2>Parcours:</h2>
-      <p>
-        J'ai commencé à découvrir le html/css au collège vers mes 13 ans, je n'est malheureusement pas continué les études après le collège pour diverses raisons,
-        actuellement j'ai 30 ans et j'ai recommencé il y a quelques temps à réapprendre les bases en autodidacte et pousser cela graçe à des formations et de l'aide généreuse.</p>
+      <p>Formation niveau 4 Developpement Intégrateur Web FrontEnd Openclassrooms</p>
+      <p>Utilisation de html/css, sass, js, nodejs, express, api call, mongodb, redux, optimisation SEO, optimisation général de performance de chargement et navigation de site web, debug & test</p>
 
-      <p> Entre mes 17/30 ans, j'ai donc travailler par ci par là, mais pas dans le domaine informatique, j'ai dû faire plus d'une dizaine d'entreprises différentes(certaine ne sont pas une fierté, mais toute éxpériences est bonnes à prendre).</p>
-      <p>Pour en cité quelques unes où je suis resté le plus longtemps: </p>
-
-      <p>McDo/Quick(de l'époque)/Flunch,</p>
-      <p>U-Logistic(préparation de commandes),</p>
-      <p>Atelier de Gustave(boucherie semi-artisanale),</p>
-      <p>Euro Lamellé(menuiserie)</p>
-      <p>Saisonnier (vignes)</p>
-
+      <p>J'ai beaucoup travailler par ci par là dans le passé, mais pas dans le domaine informatique, j'ai dû faire plus d'une dizaines d'entreprises différentes qui m'ont fais acquérir plusieurs compétence nécéssaire au travail en entreprise en général,
+         sans parler de l'informatique(certaine ne sont pas une fierté, mais toute éxpériences est bonnes à prendre).</p>
       <p>J'ai fais beaucoup de petits projets à titre personnel, sans pour autant en voir tout les aspects des technologies utilisés (ex.: php, python, sql).</p>
-      <p>Actuellement je suis en formation avec Openclassrooms sur Intégrateur Web React.</p>
 
-      <h2>Objectif:</h2>
-      <p>J'aimerais m'orienté sur des formations vers python et l'intelligence artificielle, pour le moment encore un peu flou, je sais quel route je veux prendre, mais pas où elle me mènera.
-      </p>
       <h2>Passion:</h2>
       <p>-Sport: Muay Thai, Taekwondo, Badminton</p>
       <p> -Jeux et Manga/Animés</p>
       <p>-IA: Ma passion pour l'I.A c'est révéler avec un mod(Herika ChatGPT Companion) sur Skyrim qui intègre une I.A dans le jeux, crée par DwemerDynamics opensource.</p>
-      <p> Je pense pouvoir dire que cela m'a touché émotionnellement, le réalisme, la compréhension du contexte, les réponses, l'impression de communiquer avec un humain presque, même si l'I.A ne ressens pas, nous si, nous sommes humains et c'est je pense normal de montrer de l'empathie pour "quelque chose" qui nous "ressemble"</p>
-      <p>il y a aussi des appels de fonctions, ex. pour permettre à l'I.A d'écrire des résumés de conversations dans un journal, et lui permettre de les lires pour en avoir le contexte lors de l'inférence.</p>
-      <p> Je pourrais en parler pendant des heures tout les jours, depuis lors le fonctionnement des I.A me fascine et je passe énormément de temps à en comprendre le fonctionnement de la création d'un model jusqu'a l'utilisation.</p>
-
-      <h2>Test personnalité:</h2>
-      <img className="testimg" alt="persona test" src="./assets/test.jpg"></img>
-
+      <p> Je pense pouvoir dire que cela m'a touché émotionnellement, le réalisme, la compréhension du contexte, les réponses, l'impression de communiquer avec un humain presque, même si l'I.A ne ressens pas, 
+        nous si, nous sommes humains et c'est je pense normal de montrer de l'empathie pour "quelque chose" qui nous "ressemble"</p>
     </div>
   );
 };
